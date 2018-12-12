99499eae728b84b0909338bb0de7506a124d4798
Michelle Beshears <meeshysu@gmail.com>
2018-12-11 19:34:38 -0600

    working on auth

--- src/App/App.js
+++ src/App/App.js
@@ -1,43 +1,30 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
+// import { Button } from 'reactstrap';
+// import logo from './logo.svg';
+import connection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 import './App.scss';
 
 // component is also a class. my class 'app' gets all the info from component.
 // where you have variables, you put {} around. look at logo for i.e.
 // you can use javascript in jsx.
 // put everything in a container bc you cannot have siblings in a div.
-class App extends Component { // you must have a render! you have to have a return.
-  // the return is basically what html that you want. it's written in jsx, the language of html?
+// you must have a render! you have to have a return.
+// the return is basically what html that you want. it's written in jsx, the language of html?
+
+// FIREBASE CALL
+class App extends Component { 
+
+
+  componentDidMount() {
+    connection();
+  }
+
   render() {
-    console.log(this); // this refers to the actual class itself
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className="btn btn-danger">Help!</button>
-          <Button
-            tag="a"
-            color="primary"
-            size="large"
-            href="google.com"
-            target="_blank"
-          >
-            Visit My Homeland
-          </Button>
-        </header>
+        <Auth />
       </div>
     );
   }
