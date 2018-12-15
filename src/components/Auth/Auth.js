64a13ccf1f49828f1f10c47ace9ae07a60f0aa15
Michelle Beshears <meeshysu@gmail.com>
2018-12-15 09:39:35 -0600

    proto-type successfully added

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import authRequests from '../../helpers/data/authRequests';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  static propTypes = {
+    isAuthenticated: PropTypes.func,
+  }
+
+  // static means that it has no interaction with the function, or whatever it is, like an object.
+  // propTypes is a reserved word; it has to be spelled the right way.
   authenticateUser = (e) => {
     e.preventDefault();
     // call authenticate method. write auth request.
