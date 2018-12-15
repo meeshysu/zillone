6432c8e6dd6c460c5f824f78861a06f8a4af53af
Michelle Beshears <meeshysu@gmail.com>
2018-12-15 11:18:12 -0600

    added some scss to listing

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import listingShape from '../../helpers/propz/listingShape';
 
 import './ListingItem.scss';
 
 class ListingItem extends React.Component {
+  static propTypes = {
+    listing: listingShape,
+  }
+
   render() {
     const { listing } = this.props;
     return (
-      <h2>{listing.address}</h2>
+      <li className="listing-item text-center">
+        <span className="col-7">{listing.address}</span>
+        <span className="col-3">{listing.price}</span>
+      </li>
     );
   }
 }
 
 export default ListingItem;
+
+// because listing is attached to the shapes, we just need to import the shape.
+// inside the shape it already has the PropTypes within. we don't need to import.
