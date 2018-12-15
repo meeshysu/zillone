e4d58db41f3c3f3019032d58b1a9624c648addc7
Michelle Beshears <meeshysu@gmail.com>
2018-12-15 12:04:09 -0600

    got the trash can up

--- src/components/ListingItem/ListingItem.js
+++ src/components/ListingItem/ListingItem.js
@@ -3,6 +3,7 @@ import listingShape from '../../helpers/propz/listingShape';
 import formatPrice from '../../helpers/formatPrice';
 
 import './ListingItem.scss';
+import authRequests from '../../helpers/data/authRequests';
 
 class ListingItem extends React.Component {
   static propTypes = {
@@ -11,10 +12,24 @@ class ListingItem extends React.Component {
 
   render() {
     const { listing } = this.props;
+    const uid = authRequests.getCurrentUid();
+
+    const makeButtons = () => {
+      if (listing.uid === uid) {
+        return (
+          <div>
+            <span className="col"><button className="btn btn-secondary fas fa-trash-alt"></button></span>
+          </div>
+        );
+      }
+      return <span className="col-2"></span>;
+    };
+
     return (
       <li className="listing-item text-center">
         <span className="col-7">{listing.address}</span>
         <span className="col-3">{formatPrice(listing.price)}</span>
+        {makeButtons()}
       </li>
     );
   }
